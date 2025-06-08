eafdbe9ce1ae8cf702c4a342b81fd103c64e0196
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-06-08 08:43:30 -0400

    spelling: c'mon
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/test/java/io/valkey/util/ClientKillerUtil.java
+++ src/test/java/io/valkey/util/ClientKillerUtil.java
@@ -7,7 +7,7 @@ public class ClientKillerUtil {
   public static void killClient(Jedis jedis, String clientName) {
     for (String clientInfo : jedis.clientList().split("\n")) {
       if (clientInfo.contains("name=" + clientName)) {
-        // Ugly, but cmon, it's a test.
+        // Ugly, but c'mon, it's a test.
         String hostAndPortString = clientInfo.split(" ")[1].split("=")[1];
         // It would be better if we kill the client by Id as it's safer but jedis doesn't implement
         // the command yet.
